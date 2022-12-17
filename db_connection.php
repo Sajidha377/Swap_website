11f4bd28abb66d954966e2febb35a359d0115ae4
M.A.Sajidha <89059993+Sajidha377@users.noreply.github.com>
2022-12-17 11:02:51 +0530

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,11 +1,12 @@
 <?php
   //database connection
-  $server   = "localhost";
-  $username = "root";
-  $password = "";
-  $db_name  = "db_swap";
+  $DB_HOST   = $_ENV["DB_HOST"];
+  $DB_USER = $_ENV["DB_USER"];
+  $DB_PASSWORD = $_ENV["DB_PASSWORD"];
+  $DB_NAME = $_ENV["DB_NAME"];
+  $DB_PORT = $_ENV["DB_PORT"];
 
-  $mysqli = new mysqli($server,$username,$password,$db_name);
+  $mysqli = new mysqli("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
 
   if($mysqli->connect_error){
     echo $mysqli->errorno . "<br />";
