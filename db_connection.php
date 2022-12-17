4429be406b07300244e9c105ff7a116c87efa671
M.A.Sajidha <89059993+Sajidha377@users.noreply.github.com>
2022-12-17 11:22:28 +0530

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -6,13 +6,5 @@
   $DB_NAME = $_ENV["DB_NAME"];
   $DB_PORT = $_ENV["DB_PORT"];
 
-  $mysqli = new mysqli("$DB_HOST","$DB_USER","$DB_NAME","$DB_PORT");
-
-  if($mysqli->connect_error){
-    echo $mysqli->errorno . "<br />";
-    echo $mysqli->error . "<br />";
-    die("connection failed");
-  }
-   //echo "connection successful";
-
+  $db = mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
  ?>
